392db8dda774235e792e3931976c6a724f1daf2f
Luca <luca.powell@journalism.cuny.edu>
2026-01-11 21:30:41 -0500

    Sunday, January 11 Updates
    Added transitions to my bar chart.
    Added title.
    Added some cosmetic changes to the bars.

--- ice_detention.js
+++ ice_detention.js
@@ -1,17 +1,17 @@
 const width = 1100;
 const height = 400;
-const margin = { top: 25, bottom: 350, left: 25, right: 60 };
+const margin = { top: 60, bottom: 350, left: 25, right: 60 };
 
 const svg = d3.select(".graphic")
   .append("svg")
   .attr("width", width + margin.left + margin.right) 
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 d3.csv("detentions.csv").then(data => {
   data.forEach(d=> d["Level A"] = +d["Level A"]);
-  data.sort((a, b) => d3.descending(a["Level A"], b["Level A"]))
+  data.sort((a, b) => d3.descending(a["Total Inmates"], b["Total Inmates"]))
 
   const x = d3.scaleBand()
     .domain(data.map(d => d.Name))
@@ -19,7 +19,7 @@ d3.csv("detentions.csv").then(data => {
     .padding(0.4);
 
   const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d["Level A"])])
+    .domain([0, d3.max(data, d => d["Total Inmates"])])
     .range([height, 0])
     .nice();
 
@@ -27,10 +27,26 @@ d3.csv("detentions.csv").then(data => {
 
   const yAxis = d3.axisLeft(y)
 
-  svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", `translate(0, ${height})`)
-  .call(xAxis);
+///Add Title
+svg.append("text")
+  .attr("x", width / 2)
+  .attr("y", -30)
+  .attr("text-anchor", "middle")
+  .style("font-size", "16px")
+  .style("font-weight", "600")
+  .text("ICE Detentions in Virginia");
+
+///Create Axes
+  svg
+  .append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .attr("opacity","0")
+    .call(xAxis)
+  .transition()
+    .duration(500)
+    .attr("opacity","1");
+
 
   svg.select(".x.axis")
   .selectAll("text")
@@ -41,15 +57,33 @@ d3.csv("detentions.csv").then(data => {
 
   svg.append("g")
     .call(yAxis)
+    .attr("opacity","0")
+  .transition()
+    .duration(500)
+    .attr("opacity","1");
 
+const totalDuration = 1000; // 1.5 seconds total
+const delayPerBar = totalDuration / data.length; ///Divides by three, since our data is three rows longth.
+
+///This is the code that builds the actual bars
   svg.selectAll(".bar")
     .data(data)
     .join("rect")
-    .attr("class", "bar")
-    .attr("x", d => x(d.Name))
-    .attr("y", d => y(d["Level A"]))
-    .attr("height", d => y(0) - y(d["Level A"]))
-    .attr("width", x.bandwidth())
-    .attr("fill", "steelblue");
+      .attr("class", "bar")
+      .attr("x", d => x(d.Name))
+      .attr("y", y(0))
+      .attr("height",0)
+      .attr("width", x.bandwidth())
+      .attr("fill", "steelblue")
+      .attr("opacity",".0")
+      .attr("rx",4)
+      .attr("ry",4)
+    .transition()
+      .delay((d, i) => i * delayPerBar)
+      .duration(600)
+      .ease(d3.easeCubicOut)
+      .attr("y", d => y(d["Total Inmates"]))
+      .attr("opacity",'1')
+      .attr("height", d => y(0) - y(d["Total Inmates"]));
 
 });
