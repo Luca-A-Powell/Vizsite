d423b2484fa0df83d7de0f7048799c17d27297a5
Luca <luca.powell@journalism.cuny.edu>
2026-01-25 15:29:12 -0500

    Motor Crashes Viz

--- Motor_Carriers/motorcarriers.js
+++ Motor_Carriers/motorcarriers.js
@@ -7,55 +7,77 @@ const svg = d3.select(".graphic")
 d3.csv("motor_carrier_crashes.csv", d => {
   return {
     CRASH_CARRIER_NAME: d.CRASH_CARRIER_NAME,
-    count: +d.count
+    count: +d.count,
+    logo: `logos/${d.CRASH_CARRIER_NAME}.jpg`
   }
 }).then(data => {
-  console.log(d3.extent(data, d=>d.count))
-  createViz(data);
+  console.log(data)
+  createViz(data.slice(0, 10));
 });
 
 // Main chart-drawing function
 const createViz = (data) => {
- 
-const xScale = d3.scaleBand()
+
+  const xScale = d3.scaleLinear()
+    .domain([844,d3.max(data, d=>d.count)])
+    .range([0,500])
+
+  const yScale = d3.scaleBand()
     .domain(data.map(d=>d.CRASH_CARRIER_NAME))
     .range([0,600])
-    .paddingInner(0.2)
+    .paddingInner(.10)
 
-const yScale = d3.scaleLinear()
-    .domain([0,11145])
-    .range([500,0])
-
-const barAndLabel = svg
+  ///Create a constant for my bar groups. This is how I access all my groups.///
+  const barGroup = svg
     .selectAll("g")
     .data(data)
     .join("g")
-      .attr("transform", d=> `translate(${xScale(d.CRASH_CARRIER_NAME)},0)`)
+      .attr("transform", d=>`translate(0, ${yScale(d.CRASH_CARRIER_NAME)})`);
 
-barAndLabel
-  .append('rect')
-      .attr("y", d=> yScale(d.count))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => 500 - yScale(d.count))
-      .attr("fill", d=>d.CRASH_CARRIER_NAME === "ESTES EXPRESS LINES" ? "orange":"skyblue");
+  ///Code for my rectangles///
+  barGroup
+    .append("rect")
+      .attr("x", 0)
+      .attr("height", yScale.bandwidth())
+      .attr("width", d=>xScale(d.count))
+      .attr("fill","Thistle");
 
-barAndLabel
-  .append("text")
-    .text(d=>d.CRASH_CARRIER_NAME)
-    .attr("y",500)
-    .attr("x", xScale.bandwidth()/2)
-    .attr("font-size", 9)
-    .attr("transform", d =>
-      `rotate(-90, ${xScale.bandwidth() / 2}, 515)`
-    )
+  ///Code for my line///
+  svg
+    .append("line")
+      .attr("x1","0")
+      .attr("x2", "0")
+      .attr("y1", "0")
+      .attr("y2", "600")
+      .attr("stroke","black")
+      .attr("stroke-width","1")
 
-const line = svg
-  .append("line")
-    .attr("x1", 0)
-    .attr("y1", 500)
-    .attr("x2", 600)
-    .attr("y2", 500)
-    .attr("stroke", "black");
+  ///Code for my text label
+ /*barGroup
+    .append("text")
+      .text(d=>d.CRASH_CARRIER_NAME)
+      .attr("x",d=>xScale(d.count) +54)
+      .attr("y", yScale.bandwidth()/2)
+      .attr("text-anchor","start")
+      .attr("font-size",12)*/
+
+  barGroup
+    .append("text")
+      .text(d=>d.count)
+      .attr("x", 4)
+      .attr("y", yScale.bandwidth()/2)
+      .attr("text-anchor","start")
+      .attr("font-size",12)
+
+  barGroup
+  .append("image")
+    .attr("href", d => d.logo)
+    .attr("x",d=>xScale(d.count) +4)               // left of the y-axis
+    .attr("y", yScale.bandwidth()/3)
+    .attr("width", yScale.bandwidth() + 10)
+    .attr("height", yScale.bandwidth()/2)
+    .attr("preserveAspectRatio", "xMidYMid meet");
+
 
 
 };
